94cbf82357b51acbc63ed6ded6058a9191637d6b
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2022-04-11 08:38:46 +0530

    add the comment to the project and finally completed

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ let ejs = require('ejs');
 let mongoose = require('mongoose');
 let session = require('express-session');
 let passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose'); // this plugin helps us to interact with database 
+// without directly dealing saving and finding values
 
+
+// using all the middlewares in express app
 let app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -18,23 +21,24 @@ app.use(session({
   secret: 'This is a test secret.',
   resave: false,
   saveUninitialized: true,
+  maxAge: 24*60*60*1000
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// mongoose configuration
 mongoose.connect('mongodb://localhost:27017/userDB');
 // creating pure mongoose Schema
 let userSchema = new mongoose.Schema ({
-	email: String,
+	username: String,
 	password: String
 });
-
 userSchema.plugin(passportLocalMongoose);
-
 let User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
 
+// passport configuration
+passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser()); 
 
@@ -48,6 +52,7 @@ app.get('/register', function(req,res){
 	res.render('register');
 });
 app.get('/secrets', function(req, res){
+	// check authentication for this route
 	if(req.isAuthenticated()){
 		res.render('secrets');
 	}
@@ -57,6 +62,7 @@ app.get('/secrets', function(req, res){
 });
 app.post('/register', function(req, res){
 
+	// registering user using mongoose passport plugin
 	User.register({username: req.body.username}, req.body.password, function(err, user) {
   		if (err) { 
   			console.log(err);
@@ -72,23 +78,28 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
 	let checkUser = new User({
-		email: req.body.username,
+		username: req.body.username,
 		password: req.body.password
 	});
 
-
+	// this function only takes schema with username field
 	req.login(checkUser, function(err){
 		if(err){
-			console.log('hello');
+			
+  			console.log('hello');
 			console.log(err);
-			res.redirect('/login');
-		}
-		if(err){
+		}else{
 			passport.authenticate('local')(req, res, function(){
   				res.redirect('/secrets');
   			});
 		}
 	});
+});
+
+app.get('/logout', function(req, res){
+	// logout route
+	req.logout();
+	res.redirect('/');
 });
 
 
