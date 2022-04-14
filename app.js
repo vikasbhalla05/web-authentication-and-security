2d491d69b5e5ff4ecb97e5f731e5c621e8bd2371
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2022-04-14 22:54:10 +0530

    add the google credential and callback url for the auth

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secret-web-authentication.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
